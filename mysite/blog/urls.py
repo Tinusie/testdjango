6ccbf3614722fe9aa95fa44089c0085cb1990ed1
Tinusie <merexwinkel@gmail.com>
2025-03-24 21:56:23 +0100

    dus8

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('templates/blog/', views.post_list, name='post_list'),
+    path('templates/', views.post_list, name='post_list'),
 ]
