b9a9bd143703a185cbc35b87309a6a2458a99c19
Tinusie <merexwinkel@gmail.com>
2025-03-24 10:53:43 +0100

    datum aangepast

--- mysite/player/models.py
+++ mysite/player/models.py
@@ -5,12 +5,13 @@ from django.utils import timezone
 # Create your models here.
 class Player(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    published_date = models.DateTimeField(blank=True, null=True)
     title = models.CharField(max_length=200, null=True)
     name = models.TextField(blank=True, null=True)
     club = models.TextField(blank=True, null=True)
     team = models.TextField(blank=True, null=True)
     age = models.CharField(max_length=110, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
